1bf0c44bc20fc1a44167bf9dfa2291001a552559
yu <18113406084@163.com>
2020-01-17 16:35:08 +0800

    70. 爬楼梯

--- 简单/70. 爬楼梯/climbStairs.js
+++ 简单/70. 爬楼梯/climbStairs.js
@@ -5,10 +5,10 @@
 // 动态规划
 let climbStairs = function (n) {
   let dp = []
-  dp[0] = 1
   dp[1] = 1
+  dp[2] = 2
 
-  for (let i = 2; i <= n; i++) {
+  for (let i = 3; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2]
   }
   return dp[n]
